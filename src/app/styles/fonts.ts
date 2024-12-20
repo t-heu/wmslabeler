3523d70cbd8181f7abbe4c326328bf53bf9b85a2
t-heu <teuzin375@gmail.com>
2024-12-20 12:09:52 -0300

    update

--- src/app/styles/fonts.ts
+++ src/app/styles/fonts.ts
@@ -10,6 +10,6 @@ const roboto = Roboto({
 })
 // define a custom local font where ConnectCode39.ttf is stored in the styles folder
 const greatVibes = localFont({ src: './fonts/ConnectCode39.ttf' })
- const code128 = localFont({ src: './fonts/CODE128.TTF' })
+ const code128 = localFont({ src: './fonts/CODE128.woff' })
  
 export { roboto, greatVibes, code128 }
