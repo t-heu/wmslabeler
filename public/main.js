bd60e629564d14c5633ee6ec4e7cb0ee8822cf09
t-heu <teuzin375@gmail.com>
2024-12-25 16:21:29 -0300

    update: icons

--- public/main.js
+++ public/main.js
@@ -5,8 +5,9 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/wmslabeler/sw.js') // Caminho para o seu arquivo sw.js
-      .then(registration => {
-        console.log('Service Worker registrado com sucesso:', registration);
+      .then(() => {
+        console.log('Service Worker registrado com sucesso:');
+        // console.log('Service Worker registrado com sucesso:', registration);
       })
       .catch(err => {
         console.error('Falha ao registrar o Service Worker:', err);
